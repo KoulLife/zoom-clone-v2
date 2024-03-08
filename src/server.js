19ef06dfdcc2e946a2c0430224e7cb147be008ee
Koul <dongik0122@gmail.com>
2024-03-09 04:52:51 +0900

    connect
    프론트와 백엔드 사이에서 socket이 연결됨

--- src/server.js
+++ src/server.js
@@ -1,7 +1,29 @@
+import * as http from "http";
 import express from "express"
+import {WebSocket, WebSocketServer} from "ws";
 
 const app = express()
 
-console.log("hello")
+// pug 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+// route 설정
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => console.log(`http://localhost:3000`);
+const server = http.createServer(app); //server에서 ws를 만들 수 있게 되었다.
+const wss = new WebSocketServer({server});  //WebSocket 서버와 http 서버 모두 돌릴 수 있게 되었다.
+
+// function handleConnection(socket){  // socket은 연결된 브라우
+//   console.log(socket)
+// }
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.send("hello");
+});
+
+server.listen(3000, handleListen);
